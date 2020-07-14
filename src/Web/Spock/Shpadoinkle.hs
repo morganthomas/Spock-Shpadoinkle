3a99870d2dbea4103b21d97419ab7ef03e40fd43
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-14 12:09:38 -0600

    initial commit

---
+++ src/Web/Spock/Shpadoinkle.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Web.Spock.Shpadoinkle ( spockShpadoinkle ) where
+
+import Control.Monad.IO.Class
+import Data.Text.Encoding (encodeUtf8)
+import Shpadoinkle
+import Shpadoinkle.Backend.Static
+import Web.Spock.Action
+
+spockShpadoinkle :: MonadIO m => IsHtml h p => IsProp p e => h a -> ActionCtxT ctx m b
+spockShpadoinkle x = do
+  setHeader "Content-Type" "text/html; charset=utf-8"
+  bytes (encodeUtf8 (renderStatic x))
