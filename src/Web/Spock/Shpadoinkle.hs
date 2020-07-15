13df4faced9db3621204369884ea50ef1e36a73b
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-07-14 23:29:48 -0600

    add explicit quantification

--- src/Web/Spock/Shpadoinkle.hs
+++ src/Web/Spock/Shpadoinkle.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
 
 module Web.Spock.Shpadoinkle ( spockShpadoinkle ) where
 
@@ -8,7 +9,7 @@ import Shpadoinkle
 import Shpadoinkle.Backend.Static
 import Web.Spock.Action
 
-spockShpadoinkle :: MonadIO m => IsHtml h p => IsProp p e => h a -> ActionCtxT ctx m b
+spockShpadoinkle :: forall m h p a e ctx b. MonadIO m => IsHtml h p => IsProp p e => h a -> ActionCtxT ctx m b
 spockShpadoinkle x = do
   setHeader "Content-Type" "text/html; charset=utf-8"
   bytes (encodeUtf8 (renderStatic x))
